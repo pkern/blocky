1680e7ef35565e262ca74b8224220e133ed5d121
Philipp Kern <pkern@google.com>
2024-05-05 14:14:55 +0200

    fix: Obfuscate the question_name field

--- querylog/logger_writer.go
+++ querylog/logger_writer.go
@@ -26,7 +26,7 @@ func (d *LoggerWriter) Write(entry *LogEntry) {
 			"response_reason": entry.ResponseReason,
 			"response_type":   entry.ResponseType,
 			"response_code":   entry.ResponseCode,
-			"question_name":   entry.QuestionName,
+			"question_name":   util.Obfuscate(entry.QuestionName),
 			"question_type":   entry.QuestionType,
 			"answer":          entry.Answer,
 			"duration_ms":     entry.DurationMs,
